792aa0ddd751702690639626eb1ab91bc7b838f3
NLT <NamibiaLT@users.noreply.github.com>
2019-06-27 21:26:20 -0700

    src file connected to scripts file

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,13 @@
+'use strict';
 
+console.log('App.js is running!');
+
+// JSX
+var template = React.createElement(
+  'p',
+  null,
+  ' Does this change?!'
+);
+var appRoot = document.getElementById('app'); // This refers to the id we gave our div tag in the indx.html file
+// ReactDom takes 2 args: 1) your jsx var 2)where you want to render the jsx
+ReactDOM.render(template, appRoot);

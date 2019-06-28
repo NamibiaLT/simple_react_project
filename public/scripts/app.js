f623ac7c6b4f5f24df6bc562f49cef6e571b1c74
NLT <NamibiaLT@users.noreply.github.com>
2019-06-27 21:00:42 -0700

    rewrite app.js file

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,13 @@
 console.log('App.js is running!');
 
 // JSX
-var template = <p> This is JSX from app.js!</p>;
+// var template = <p> This is JSX from app.js!</p>;
+var template = React.createElement(
+  "h1",
+  { id: "someid" },
+  "Something New"
+);
+
 var appRoot = document.getElementById('app'); // This refers to the id we gave our div tag in the indx.html file
 // ReactDom takes 2 args: 1) your jsx var 2)where you want to render the jsx
 ReactDOM.render(template, appRoot);

97cbe73078045ffaba037c8337188be45ae5af77
NLT <NamibiaLT@users.noreply.github.com>
2019-06-27 21:39:46 -0700

    reformat

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 console.log('App.js is running!');
 
 // JSX
-var template = <div><h1>Indecision App</h1><p> Some information </p></div>;
+var template = (
+  <div>
+    <h1>Indecision App</h1>
+    <p> Some information </p>
+  </div>
+);
 var appRoot = document.getElementById('app'); // This refers to the id we gave our div tag in the indx.html file
 // ReactDom takes 2 args: 1) your jsx var 2)where you want to render the jsx
 ReactDOM.render(template, appRoot);

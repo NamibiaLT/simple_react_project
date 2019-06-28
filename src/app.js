d95e6f75fba1d5a82b643f2f5f3fbbc40c14e621
NLT <NamibiaLT@users.noreply.github.com>
2019-06-27 21:50:33 -0700

    add templateTwo

--- src/app.js
+++ src/app.js
@@ -7,6 +7,14 @@ var template = (
     <p> Some information </p>
   </div>
 );
+
+var templateTwo = (
+  <div>
+    <h1>Namibia</h1>
+    <p>Age: 1000</p>
+    <p>Location: SF</p>
+  </div>
+);
 var appRoot = document.getElementById('app'); // This refers to the id we gave our div tag in the indx.html file
 // ReactDom takes 2 args: 1) your jsx var 2)where you want to render the jsx
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);

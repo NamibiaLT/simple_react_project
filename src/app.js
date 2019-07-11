01559e3485eef7a6f09b6fe257a5d1671ee9b062
NLT <NamibiaLT@users.noreply.github.com>
2019-07-11 14:41:07 -0700

    add variable

--- src/app.js
+++ src/app.js
@@ -55,6 +55,7 @@ var templateTwo = (
     {getLocation(user.location)}
   </div>
 );
+
 var appRoot = document.getElementById('app'); // This refers to the id we gave our div tag in the indx.html file
 // ReactDom takes 2 args: 1) your jsx var 2)where you want to render the jsx
 ReactDOM.render(templateThree, appRoot);

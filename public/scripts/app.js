dd6e07befa34b47d15cf74e5d0a89b091faffdab
NLT <NamibiaLT@users.noreply.github.com>
2019-07-11 14:05:29 -0700

    render new template

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,11 +3,61 @@
 console.log('App.js is running!');
 
 // JSX
+var appInfo = {
+  title: 'Indecision App',
+  subtitle: 'Hey, you are awesome and you got this'
+};
+
 var template = React.createElement(
-  'p',
+  'div',
   null,
-  ' Does this change?!'
+  React.createElement(
+    'h1',
+    null,
+    appInfo.title
+  ),
+  React.createElement(
+    'p',
+    null,
+    ' ',
+    appInfo.subtitle,
+    ' '
+  )
+);
+
+var user = {
+  name: 'Torri',
+  age: 1000,
+  location: 'San Francisco'
+};
+
+function getLocation(location) {
+  if (location) {
+    return React.createElement(
+      'p',
+      null,
+      'Location: ',
+      location
+    );
+  } // else: it implicitly returns undefined
+}
+
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    user.name ? user.name : 'Anonymous'
+  ),
+  React.createElement(
+    'p',
+    null,
+    'Age: ',
+    user.age
+  ),
+  getLocation(user.location)
 );
 var appRoot = document.getElementById('app'); // This refers to the id we gave our div tag in the indx.html file
 // ReactDom takes 2 args: 1) your jsx var 2)where you want to render the jsx
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,20 +1,51 @@
 console.log('App.js is running!');
 
 // JSX
+var appInfo = {
+  title: 'Indecision App',
+  subtitle: 'Hey, you are awesome and you got this'
+};
+
 var template = (
   <div>
-    <h1>Indecision App</h1>
-    <p> Some information </p>
+    <h1>{appInfo.title}</h1>
+    <p> {appInfo.subtitle} </p>
   </div>
 );
 
+var user = {
+  name: 'Torri',
+  age: 1000,
+  location: 'San Francisco'
+};
+
+var dreamJob = {
+  title: 'Software Engineer',
+  start: 'July 31st',
+  location: 'San Fran'
+}
+
+function getLocation(location) {
+  if (location) {
+    return <p>Location: {location}</p>;
+  } // else: it implicitly returns undefined
+}
+
+var template = (
+  <div>
+    <h1>Dream Job details:</h1>
+    <p>Job Title: {dreamJob.title}</p>
+    <p>My dream job starts on: {dreamJob.start}</p>
+    <p>My dream job is located in: {dreamJob.location}</p>
+  </div>
+);
 var templateTwo = (
   <div>
-    <h1>Namibia</h1>
-    <p>Age: 1000</p>
-    <p>Location: SF</p>
+    <h1>{user.name? user.name : 'Anonymous'}</h1>
+    <p>Age: {user.age}</p>
+    {getLocation(user.location)}
   </div>
 );
 var appRoot = document.getElementById('app'); // This refers to the id we gave our div tag in the indx.html file
 // ReactDom takes 2 args: 1) your jsx var 2)where you want to render the jsx
-ReactDOM.render(templateTwo, appRoot);
+ReactDOM.render(template, appRoot);

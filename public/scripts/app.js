ae12ebb36b57138dd05a754e0a9eee68cb9d256e
NLT <NamibiaLT@users.noreply.github.com>
2019-07-13 12:19:31 -0700

    created ES6 functions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,113 +1,15 @@
-'use strict';
+"use strict";
 
-console.log('App.js is running!');
-
-// JSX
-var appInfo = {
-  title: 'Indecision App',
-  subtitle: 'Hey, you are awesome and you got this',
-  options: []
+var square = function square(x) {
+  return x * x;
 };
 
-var template = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    appInfo.title
-  ),
-  appInfo.subtitle && React.createElement(
-    'p',
-    null,
-    ' ',
-    appInfo.subtitle,
-    ' '
-  ),
-  appInfo.options.length > 0 ? "Here are your options" : "No options"
-);
+// const squareArrow = (x) => {
+//   return x * x;
+// };
 
-var user = {
-  name: 'Torri',
-  age: 1000,
-  location: 'San Francisco'
+var squareArrow = function squareArrow(x) {
+  return x * x;
 };
 
-var dreamJob = {
-  title: ' Software Engineer',
-  start: 'July 31st,2019',
-  location: 'San Fran'
-};
-
-function getLocation(location) {
-  if (location) {
-    return React.createElement(
-      'p',
-      null,
-      'Location: ',
-      location
-    );
-  } // else: it implicitly returns undefined
-}
-
-function getTitle(title) {
-  if (title) {
-    return React.createElement(
-      'p',
-      null,
-      ' Job title:',
-      title
-    );
-  } else {
-    return 'Unknown';
-  }
-}
-
-var templateThree = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    'Dream Job details:'
-  ),
-  React.createElement(
-    'p',
-    null,
-    getTitle(dreamJob.title)
-  ),
-  React.createElement(
-    'p',
-    null,
-    'My dream job starts on: ',
-    dreamJob.start
-  ),
-  React.createElement(
-    'p',
-    null,
-    'My dream job is located in: ',
-    dreamJob.location
-  )
-);
-
-var templateTwo = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    user.name ? user.name : 'Anonymous'
-  ),
-  user.age && user.age >= 18 && React.createElement(
-    'p',
-    null,
-    'Age: ',
-    user.age
-  ),
-  getLocation(user.location)
-) // user.age logical && operator first checks if there is an age, then if that age is greater than 18, then if it is prints out the age
-;
-
-var appRoot = document.getElementById('app'); // This refers to the id we gave our div tag in the indx.html file
-// ReactDom takes 2 args: 1) your jsx var 2)where you want to render the jsx
-ReactDOM.render(templateThree, appRoot);
+console.log(squareArrow(9));

b0f3121f36fcaf7349af41ddc8a8d8ad590b2288
NLT <NamibiaLT@users.noreply.github.com>
2019-07-13 10:56:05 -0700

    block level scope

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -17,3 +17,13 @@ function getPetName() {
 
 var petName = getPetName();
 console.log(petName);
+
+// Block scoping:
+
+var fullName = 'Jerry Garcia';
+if (fullName) {
+  var firstName = fullName.split(' ')[0];
+  console.log(firstName);
+}
+
+console.log(firstName);
--- src/playground/es6-let-const.js
+++ src/playground/es6-let-const.js
@@ -15,3 +15,13 @@ function getPetName() {
 
 const petName = getPetName();
 console.log(petName);
+
+// Block scoping:
+
+var fullName = 'Jerry Garcia'
+if (fullName) {
+  var firstName = fullName.split(' ')[0];
+  console.log(firstName);
+}
+
+console.log(firstName);

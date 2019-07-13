af7b651d891b0a33e92dc7191f982d33966ad054
NLT <NamibiaLT@users.noreply.github.com>
2019-07-12 17:02:05 -0700

    create test function

---
+++ src/playground/es6-let-const.js
@@ -0,0 +1,17 @@
+var nameVar = 'Andrew';
+nameVar = 'Mike';
+console.log('nameVar', nameVar);
+
+let nameLet = "Jet";
+console.log('nameLet', nameLet)
+
+const nameConst = 'Fran';
+console.log('nameConst', nameConst);
+
+function getPetName() {
+  var petName = 'Hugo';
+  return petName;
+}
+
+getPetName();
+console.log(petName);

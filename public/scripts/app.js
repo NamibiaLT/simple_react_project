5f0376daefe23956d6771643b92f160ac45797bb
NLT <NamibiaLT@users.noreply.github.com>
2019-07-13 11:14:01 -0700

    change var to const

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,31 +1,113 @@
 'use strict';
 
-var nameVar = 'Andrew';
-nameVar = 'Mike';
-console.log('nameVar', nameVar);
+console.log('App.js is running!');
 
-var nameLet = "Jet";
-console.log('nameLet', nameLet);
+// JSX
+var appInfo = {
+  title: 'Indecision App',
+  subtitle: 'Hey, you are awesome and you got this',
+  options: []
+};
 
-var nameConst = 'Fran';
-console.log('nameConst', nameConst);
+var template = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    appInfo.title
+  ),
+  appInfo.subtitle && React.createElement(
+    'p',
+    null,
+    ' ',
+    appInfo.subtitle,
+    ' '
+  ),
+  appInfo.options.length > 0 ? "Here are your options" : "No options"
+);
 
-function getPetName() {
-  var petName = 'Hugo';
-  return petName;
+var user = {
+  name: 'Torri',
+  age: 1000,
+  location: 'San Francisco'
+};
+
+var dreamJob = {
+  title: ' Software Engineer',
+  start: 'July 31st,2019',
+  location: 'San Fran'
+};
+
+function getLocation(location) {
+  if (location) {
+    return React.createElement(
+      'p',
+      null,
+      'Location: ',
+      location
+    );
+  } // else: it implicitly returns undefined
 }
 
-var petName = getPetName();
-console.log(petName);
-
-// Block scoping:
-
-var fullName = 'Jerry Garcia';
-var firstName = void 0;
-
-if (fullName) {
-  firstName = fullName.split(' ')[0];
-  console.log(firstName);
+function getTitle(title) {
+  if (title) {
+    return React.createElement(
+      'p',
+      null,
+      ' Job title:',
+      title
+    );
+  } else {
+    return 'Unknown';
+  }
 }
 
-console.log(firstName);
+var templateThree = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    'Dream Job details:'
+  ),
+  React.createElement(
+    'p',
+    null,
+    getTitle(dreamJob.title)
+  ),
+  React.createElement(
+    'p',
+    null,
+    'My dream job starts on: ',
+    dreamJob.start
+  ),
+  React.createElement(
+    'p',
+    null,
+    'My dream job is located in: ',
+    dreamJob.location
+  )
+);
+
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    user.name ? user.name : 'Anonymous'
+  ),
+  user.age && user.age >= 18 && React.createElement(
+    'p',
+    null,
+    'Age: ',
+    user.age
+  ),
+  getLocation(user.location)
+) // user.age logical && operator first checks if there is an age, then if that age is greater than 18, then if it is prints out the age
+;
+
+var appRoot = document.getElementById('app'); // This refers to the id we gave our div tag in the indx.html file
+// ReactDom takes 2 args: 1) your jsx var 2)where you want to render the jsx
+ReactDOM.render(templateThree, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 console.log('App.js is running!');
 
 // JSX
-var appInfo = {
+const appInfo = {
   title: 'Indecision App',
   subtitle: 'Hey, you are awesome and you got this',
   options: []
 };
 
-var template = (
+const template = (
   <div>
     <h1>{appInfo.title}</h1>
     {appInfo.subtitle && <p> {appInfo.subtitle} </p>}
@@ -15,14 +15,14 @@ var template = (
   </div>
 );
 
-var user = {
+const user = {
   name: 'Torri',
   age: 1000,
   location: 'San Francisco'
 };
 
-var dreamJob = {
-  title: 'Software Engineer',
+const dreamJob = {
+  title: ' Software Engineer',
   start: 'July 31st,2019',
   location: 'San Fran'
 };
@@ -41,16 +41,16 @@ function getTitle(title) {
   }
 }
 
-var templateThree = (
+const templateThree = (
   <div>
     <h1>Dream Job details:</h1>
-    <p>Job Title: {getTitle(dreamJob.title)}</p>
+    <p>{getTitle(dreamJob.title)}</p>
     <p>My dream job starts on: {dreamJob.start}</p>
     <p>My dream job is located in: {dreamJob.location}</p>
   </div>
 );
 
-var templateTwo = (
+const templateTwo = (
   <div>
     <h1>{user.name ? user.name : 'Anonymous'}</h1>
     {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
@@ -58,6 +58,6 @@ var templateTwo = (
   </div> // user.age logical && operator first checks if there is an age, then if that age is greater than 18, then if it is prints out the age
 );
 
-var appRoot = document.getElementById('app'); // This refers to the id we gave our div tag in the indx.html file
+const appRoot = document.getElementById('app'); // This refers to the id we gave our div tag in the indx.html file
 // ReactDom takes 2 args: 1) your jsx var 2)where you want to render the jsx
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateThree, appRoot);
